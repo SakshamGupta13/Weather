ce6d7a78f1f1567c858785bbe7afb17ec617de9e
Saksham <guptasaksham795@gmail.com>
2025-05-27 14:03:12 +0530

    updated

--- script.js
+++ script.js
@@ -51,19 +51,19 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/assets/cloud.png";
+            weather_img.src = "/galary/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "/galary/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/assets/rain.png";
+            weather_img.src = "/galary/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/assets/mist.png";
+            weather_img.src = "/galary/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/assets/snow.png";
+            weather_img.src = "/galary/snow.png";
             break;
 
     }
